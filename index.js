81ee95e60cf92c0c09444c10bceae06f2faa7a68
Admin <vanpho02@gmail.com>
2016-12-12 08:41:06 +0700

    start 2

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 var express = require('express');
+var session = require('express-session');
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 app.listen(3000, () => console.log('Server started'));
 
+app.use(session({
+  secret: 'j^gYTF^3278O((7))',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {maxAge: 3000}
+}));
+
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.get('/muave', (req, res) => {
+  req.session.daMuaVe = 1;
+  res.send('Ban da mua ve');
+});
+
+app.get('/vaorap', (req, res) => {
+  if(req.session.daMuaVe){
+    req.session.daMuaVe++;
+    return res.send('Welcome');
+  }
+  res.send('Ban hay di mua ve');
 });
